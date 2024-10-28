eb3db91bb887ba2b4c25578e5cf74096ea2401f8
qwe1777 <qwe1277777@gmail.com>
2024-10-28 11:17:44 +0300

    Update and rename VM_clear to VM_clear.tf

--- VM_clear.tf
+++ VM_clear.tf
@@ -1,52 +1,13 @@
-data "yandex_compute_image" "ubuntu_image4" {
-  family = "ubuntu-2004-lts"
-}
- 
-resource "yandex_compute_instance" "vm-test4" {
-  name = "test4"
- 
-  resources {
-    cores  = 4
-    memory = 8
-  }
- 
-  boot_disk {
-    initialize_params {
-      image_id = data.yandex_compute_image.ubuntu_image.id
-    }
-  }
- 
-  network_interface {
-    subnet_id = yandex_vpc_subnet.subnet_terraform.id
-    nat       = true
-  }
- 
-  metadata = {
-    user-data = "${file("./meta.yml")}"
-  }
-
-resource "yandex_vpc_network" "network_terraform4" {
-  name = "net_terraform"
-}
-
-resource "yandex_vpc_subnet" "subnet_terraform4" {
-  name           = "sub_terraform"
-  zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.network_terraform.id
-  v4_cidr_blocks = ["192.168.15.0/24"]
-}
-
-
-data "yandex_compute_image" "ubuntu_image5" {
+data "yandex_compute_image" "ubuntu_image" {
   family = "ubuntu-2004-lts"
 }
 
-resource "yandex_compute_instance" "vm-test5" {
-  name = "test5"
+resource "yandex_compute_instance" "vm-test1" {
+  name = "test1"
 
   resources {
-    cores  = 4
-    memory = 8
+    cores  = 2
+    memory = 2
   }
 
   boot_disk {
@@ -64,13 +25,71 @@ resource "yandex_compute_instance" "vm-test5" {
     user-data = "${file("./meta.yml")}"
   }
 
-resource "yandex_vpc_network" "network_terraform5" {
+ connection {
+    type        = "ssh"
+    user        = "qwe"
+    private_key = file("./key")
+    host        = self.network_interface[0].nat_ip_address
+  }
+
+
+
+}
+
+
+resource "yandex_vpc_network" "network_terraform" {
   name = "net_terraform"
 }
 
-resource "yandex_vpc_subnet" "subnet_terraform5" {
+resource "yandex_vpc_subnet" "subnet_terraform" {
   name           = "sub_terraform"
   zone           = "ru-central1-a"
   network_id     = yandex_vpc_network.network_terraform.id
   v4_cidr_blocks = ["192.168.15.0/24"]
 }
+
+
+data "yandex_compute_image" "ubuntu_image2" {
+  family = "ubuntu-2004-lts"
+}
+
+resource "yandex_compute_instance" "vm-test2" {
+  name = "test2"
+
+  resources {
+    cores  = 4
+    memory = 8
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = data.yandex_compute_image.ubuntu_image.id
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet_terraform.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.yml")}"
+  }
+
+ connection {
+  }
+
+
+
+}
+
+
+resource "yandex_vpc_network" "network_terraform2" {
+  name = "net_terraform2"
+}
+
+resource "yandex_vpc_subnet" "subnet_terraform2" {
+  name           = "sub_terraform2"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network_terraform.id
+  v4_cidr_blocks = ["192.168.17.0/24"]
